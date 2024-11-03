2c9fd61f95e4d81f755f28f51f592742e855d478
Isabelle Fonseca <isabelle7536@gmail.com>
2024-11-03 01:49:28 -0300

    Adicionar componente principal App com Header e MainContent

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
 
-function App() {
+import './index.css';
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col">
+      <Header />
+      <MainContent />
     </div>
   );
-}
+};
 
 export default App;
+
